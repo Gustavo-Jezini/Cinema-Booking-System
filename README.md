26bc4ae0dfb6aa76f4c5d8000d3c692cd652028b
Gustavo Jezini <gustavojezini@gmail.com>
2026-04-27 12:09:24 -0300

    Implement booking system with concurrency handling and tests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Cinema-Booking-System
+
+## Focus
+We're going to focus on the problem of double bookings,
+
+Handle for when a user starts booking at the same time another users.
+We create a "hold" on that seat.
+
+Solution: Pessimistic solution
---
+++ internal/booking/concurrent_store.go
@@ -0,0 +1,39 @@
+package booking
+
+import "sync"
+
+type ConcurrenceStore struct {
+	bookings map[string]Booking
+	sync.RWMutex
+}
+
+func NewConcurrenceStore() *ConcurrenceStore {
+	return &ConcurrenceStore{
+		bookings: map[string]Booking{},
+	}
+}
+
+func (s *ConcurrenceStore) Book(b Booking) error {
+	s.Lock()
+	defer s.Unlock()
+
+	if _, exists := s.bookings[b.SeatID]; exists {
+		return ErrSeatAlreadyBooked
+	}
+	
+	s.bookings[b.SeatID] = b
+	return nil
+}
+
+func (s *ConcurrenceStore) ListBookings(movieID string) []Booking {
+	s.RLock()
+	defer s.RUnlock()
+	
+	var result []Booking
+	for _, b := range s.bookings {
+		if b.MovieID == movieID {
+			result = append(result, b)
+		}
+	}
+	return result
+}
