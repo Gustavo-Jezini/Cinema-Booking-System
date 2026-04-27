b0b64e4bf234879edbe48e0d2e72aa65978670a5
Gustavo Jezini <gustavojezini@gmail.com>
2026-04-27 15:16:53 -0300

    add /movies route

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /movies", listMovies)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var movies = []movieResponse{
+	{ID: "inception", Title: "Inception", Rows: 5, SeatsPerRows: 8},
+	{ID: "dune", Title: "Dune: Part Two", Rows: 4, SeatsPerRows: 6},
+}
+
+func listMovies(w http.ResponseWriter, r *http.Request) {
+	WriteJson(w, http.StatusOK, movies)
+}
+
+func WriteJson(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	json.NewEncoder(w).Encode(v)
+}
+
+type movieResponse struct {
+	ID           string `json:"id"`
+	Title        string `json:"title"`
+	Rows         int    `json:"rows"`
+	SeatsPerRows int    `json:"seats_per_row"`
 }
