04d84802e6b85ffbe18e744522119bcf6bd54542
Gustavo Jezini <gustavojezini@gmail.com>
2026-04-27 16:12:25 -0300

    fav movies

--- cmd/main.go
+++ cmd/main.go
@@ -31,8 +31,8 @@ func main() {
 }
 
 var movies = []movieResponse{
-	{ID: "inception", Title: "Inception", Rows: 5, SeatsPerRows: 8},
-	{ID: "dune", Title: "Dune: Part Two", Rows: 4, SeatsPerRows: 6},
+	{ID: "blood", Title: "There Will be Blood", Rows: 5, SeatsPerRows: 8},
+	{ID: "budapest", Title: "The Great Hotel: Budapest", Rows: 4, SeatsPerRows: 6},
 }
 
 func listMovies(w http.ResponseWriter, r *http.Request) {
